a3b5c6713f3f5efe6df43c50af1797b6eeb9a206
Harbans <84002120+solo-Mrblack@users.noreply.github.com>
2021-12-09 16:32:41 +0530

    del img form dir and red to alluser

--- inventory management system/delete.php
+++ inventory management system/delete.php
@@ -8,10 +8,17 @@
 	$delUser = "DELETE FROM `users` WHERE `id`='".$userid."'";
 	$delRes = $conn->query($delUser);
 
-	$imgRes = $delRes->fetch_assoc();
+
+	$selImg = "SELECT * FROM `users` WHERE `id`='".$userid."'";
+	$selRsss = $conn->query($selImg);
+	
+	$imgRes = $selRsss->fetch_assoc();
 	$img = $imgRes['img'];
-	if(unlink($img)){
+
+	if(unlink($img) || $delRes){
+
 		header('location:allUsers.php');
+
 		echo "deleted successfully!!";
 	}else{
 		echo "unable to delete the data!!";
